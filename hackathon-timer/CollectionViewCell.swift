4a53e5a26bf3553ee6b2a268e29642af921c22cc
Arjun Sarode <sarodearjun57@gmail.com>
2014-10-19 05:19:13 -0400

    Finish initial version

---
+++ hackathon-timer/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  hackathon-timer
+//
+//  Created by Arjun Sarode on 10/19/14.
+//  Copyright (c) 2014 arjun. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    
+}
